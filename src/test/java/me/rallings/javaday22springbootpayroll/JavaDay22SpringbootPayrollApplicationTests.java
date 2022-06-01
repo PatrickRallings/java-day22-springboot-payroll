01f1312912453802742dbd7afa5a053e5680bfd9
Patrick Rallings <Patrick.Rallings@gmail.com>
2022-06-01 08:54:01 -0400

    Initial commit

---
+++ src/test/java/me/rallings/javaday22springbootpayroll/JavaDay22SpringbootPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package me.rallings.javaday22springbootpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDay22SpringbootPayrollApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
